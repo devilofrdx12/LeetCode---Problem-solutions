0a01ea6db6f8b066462fed6148c66e23ce5a2542
Hari Karthick G <122606553+devilofrdx12@users.noreply.github.com>
2026-06-16 16:20:15 +0530

    Implement method to find max consecutive ones

---
+++ Qno_485.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int min = 0;
+        int max = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                min++;
+                if (min>max)
+                {
+                    max = min;
+                }
+            }
+            else
+            {
+                min = 0;
+            }
+        }
+        return max;
+    }
+}
